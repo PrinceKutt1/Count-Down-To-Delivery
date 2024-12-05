f1f9044c6c43f4d64a1ba172c082976e43bdbac0
Kutorglo Prince Wells <87288156+PrinceKutt1@users.noreply.github.com>
2024-12-05 15:09:11 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-  const endDate = new Date("March 17, 2025 10:00:00").getTime();
+  const endDate = new Date("March 10, 2025 10:00:00").getTime();
   const now = new Date().getTime();
 
   const difference = endDate - now;
